9ba5b455ce116f1555b26a9532fb3da6186c98cf
charan2000 <charanbhavisetti2000@gmail.com>
2022-07-20 00:22:53 +0530

    addin images and audio to button clicks

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,6 +1,18 @@
 
-document.querySelector("button").addEventListener("click", handleClick);
+for( let i=0;i< document.querySelectorAll(".drum").length; i++ ){
 
-function handleClick(){
-    alert("I got Clicked")
+
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+
+    });
+
 }
+
+
+
+
+ 
